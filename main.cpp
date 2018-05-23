7ae4a05806dc751bbc8765a3dbc820fe5010664e
Ushitora Anqou <ushitora_anqou@yahoo.co.jp>
2018-05-23 15:16:01 +0900

    LinearとReLUとSoftmaxCrossEntropy層を追加。

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
+#include <cmath>
 #include <iostream>
 #include <tuple>
 
+///////////////////////
 // N-by-M matrix
+//////////////////////
+// Vector is an alias of special Matrix
+template <size_t N, size_t M>
+struct Matrix;
+template <size_t N>
+using Vector = Matrix<1, N>;
 template <size_t N, size_t M>
 struct Matrix {
     float data[N][M];
@@ -28,11 +36,47 @@ struct Matrix {
     constexpr float& operator()(size_t i) { return data[0][i]; }
     constexpr float operator[](size_t i) const { return (*this)(i); }
     constexpr float& operator[](size_t i) { return (*this)(i); }
-};
 
-// Vector is an alias of special Matrix
-template <size_t N>
-using Vector = Matrix<1, N>;
+    template <size_t rN>
+    constexpr Matrix<N, M> operator+(const Matrix<rN, M>& rhs) const
+    {
+        static_assert(rN == 1 || rN == N);
+        if constexpr (rN == 1) {
+            // broadcast
+            Matrix<N, M> ret;
+            for (size_t i = 0; i < N; i++)
+                for (size_t j = 0; j < M; j++)
+                    ret(i, j) = (*this)(i, j) + rhs[j];
+            return ret;
+        }
+        else {
+            Matrix<N, M> ret;
+            for (size_t i = 0; i < N; i++)
+                for (size_t j = 0; j < M; j++)
+                    ret(i, j) = (*this)(i, j) + rhs(i, j);
+            return ret;
+        }
+    }
+
+    constexpr Matrix<N, M> operator*(float scalar) const
+    {
+        Matrix<N, M> ret;
+        for (size_t i = 0; i < N; i++)
+            for (size_t j = 0; j < M; j++) ret(i, j) = (*this)(i, j) * scalar;
+        return ret;
+    }
+
+    template <size_t L>
+    constexpr Matrix<N, L> dot(const Matrix<M, L>& rhs) const
+    {
+        Matrix<N, L> ret;
+        for (size_t i = 0; i < N; i++)
+            for (size_t j = 0; j < L; j++)
+                for (size_t k = 0; k < M; k++)
+                    ret(i, j) += (*this)(i, k) * rhs(k, j);
+        return ret;
+    }
+};
 
 template <size_t lN, size_t lM, size_t rN, size_t rM>
 constexpr bool operator==(const Matrix<lN, lM>& lhs, const Matrix<rN, rM>& rhs)
@@ -50,6 +94,70 @@ constexpr bool operator!=(const Matrix<lN, lM>& lhs, const Matrix<rN, rM>& rhs)
 {
     return !(lhs == rhs);
 }
+
+/////////////////////
+//// NN component
+/////////////////////
+template <size_t InSize, size_t OutSize>
+struct Linear {
+    Matrix<InSize, OutSize> W;
+    Vector<OutSize> b;
+
+    constexpr Linear() : W{}, b{} {}
+
+    template <size_t BatchSize>
+    constexpr Matrix<BatchSize, OutSize> forward(
+        const Matrix<BatchSize, InSize>& src)
+    {
+        return src.dot(W) + b;
+    }
+};
+
+template <size_t Size>
+struct ReLU {
+    template <size_t BatchSize>
+    constexpr Matrix<BatchSize, Size> forward(Matrix<BatchSize, Size> src)
+    {
+        for (size_t i = 0; i < BatchSize; i++)
+            for (size_t j = 0; j < Size; j++)
+                if (src(i, j) < 0) src(i, j) = 0;
+        return src;
+    }
+};
+
+template <size_t InSize>
+struct SoftmaxCrossEntropy {
+    template <size_t BatchSize>
+    constexpr float forward(Matrix<BatchSize, InSize> src,
+                            const Matrix<BatchSize, InSize>& t)
+    {
+        // process softmax
+        for (size_t i = 0; i < BatchSize; i++) {
+            // c = max src[i]
+            float c = src(i, 0);
+            for (size_t j = 0; j < InSize; j++) c = std::max(c, src(i, j));
+
+            // src[i] = exp(src[i] - c)
+            // s = sum src[i]
+            float s = 0;
+            for (size_t j = 0; j < InSize; j++) {
+                src(i, j) = std::exp(src(i, j) - c);
+                s += src(i, j);
+            }
+
+            // src[i] /= s
+            for (size_t j = 0; j < InSize; j++) src(i, j) /= s;
+        }
+
+        // process cross-entropy
+        float loss_sum = 0;
+        for (size_t i = 0; i < BatchSize; i++)
+            for (size_t j = 0; j < InSize; j++)
+                loss_sum += t(i, j) * std::log(src(i, j));
+
+        return -loss_sum / BatchSize;
+    }
+};
 
 #include "test.cpp"
 
--- test.cpp
+++ test.cpp
@@ -83,6 +83,104 @@ constexpr void test_matrix_transposed()
     static_assert(std::get<0>(ret)(1, 1) == std::get<1>(ret)(1, 1));
 }
 
+constexpr auto test_matrix_add_detail()
+{
+    Matrix<2, 2> mat, mat2, mat3;
+    mat(0, 0) = 0;
+    mat(0, 1) = 1;
+    mat(1, 0) = 2;
+    mat(1, 1) = 3;
+    mat2(0, 0) = 1;
+    mat2(0, 1) = 2;
+    mat2(1, 0) = 3;
+    mat2(1, 1) = 4;
+    mat3(0, 0) = 1;
+    mat3(0, 1) = 3;
+    mat3(1, 0) = 5;
+    mat3(1, 1) = 7;
+
+    return std::make_tuple(mat + mat2, mat3);
+}
+
+constexpr void test_matrix_add()
+{
+    constexpr auto ret = test_matrix_add_detail();
+    static_assert(std::abs(std::get<0>(ret)(0, 0) - std::get<1>(ret)(0, 0)) <
+                  1e-5);
+    static_assert(std::abs(std::get<0>(ret)(0, 1) - std::get<1>(ret)(0, 1)) <
+                  1e-5);
+    static_assert(std::abs(std::get<0>(ret)(1, 0) - std::get<1>(ret)(1, 0)) <
+                  1e-5);
+    static_assert(std::abs(std::get<0>(ret)(1, 1) - std::get<1>(ret)(1, 1)) <
+                  1e-5);
+}
+
+constexpr auto test_matrix_mul_detail()
+{
+    Matrix<2, 2> mat, mat2;
+    mat(0, 0) = 0;
+    mat(0, 1) = 1;
+    mat(1, 0) = 2;
+    mat(1, 1) = 3;
+    mat2(0, 0) = 0;
+    mat2(0, 1) = 2;
+    mat2(1, 0) = 4;
+    mat2(1, 1) = 6;
+
+    return std::make_tuple(mat * 2, mat2);
+}
+
+constexpr void test_matrix_mul()
+{
+    constexpr auto ret = test_matrix_mul_detail();
+    static_assert(std::abs(std::get<0>(ret)(0, 0) - std::get<1>(ret)(0, 0)) <
+                  1e-5);
+    static_assert(std::abs(std::get<0>(ret)(0, 1) - std::get<1>(ret)(0, 1)) <
+                  1e-5);
+    static_assert(std::abs(std::get<0>(ret)(1, 0) - std::get<1>(ret)(1, 0)) <
+                  1e-5);
+    static_assert(std::abs(std::get<0>(ret)(1, 1) - std::get<1>(ret)(1, 1)) <
+                  1e-5);
+}
+
+constexpr auto test_matrix_dot_detail()
+{
+    Matrix<2, 3> mat;
+    Matrix<3, 2> mat2;
+    Matrix<2, 2> ans;
+    mat(0, 0) = 0;
+    mat(0, 1) = 1;
+    mat(0, 2) = -1;
+    mat(1, 0) = 2;
+    mat(1, 1) = -3;
+    mat(1, 2) = 3;
+    mat2(0, 0) = 0;
+    mat2(0, 1) = 1;
+    mat2(1, 0) = 0.5;
+    mat2(1, 1) = 3;
+    mat2(2, 0) = 1;
+    mat2(2, 1) = -3;
+    ans(0, 0) = -0.5;
+    ans(0, 1) = 6;
+    ans(1, 0) = 1.5;
+    ans(1, 1) = -16;
+
+    return std::make_tuple(mat.dot(mat2), ans);
+}
+
+constexpr void test_matrix_dot()
+{
+    constexpr auto ret = test_matrix_dot_detail();
+    static_assert(std::abs(std::get<0>(ret)(0, 0) - std::get<1>(ret)(0, 0)) <
+                  1e-5);
+    static_assert(std::abs(std::get<0>(ret)(0, 1) - std::get<1>(ret)(0, 1)) <
+                  1e-5);
+    static_assert(std::abs(std::get<0>(ret)(1, 0) - std::get<1>(ret)(1, 0)) <
+                  1e-5);
+    static_assert(std::abs(std::get<0>(ret)(1, 1) - std::get<1>(ret)(1, 1)) <
+                  1e-5);
+}
+
 constexpr auto test_vector1_detail()
 {
     Vector<2> vec;
@@ -112,4 +210,91 @@ constexpr void test_vector2()
 {
     constexpr auto ret = test_vector2_detail();
     static_assert(std::get<0>(ret) != std::get<1>(ret));
+}
+
+constexpr auto test_linear1_detail()
+{
+    Matrix<2, 3> W;
+    W(0, 0) = 0;
+    W(0, 1) = 1;
+    W(0, 2) = 1;
+    W(1, 0) = 2;
+    W(1, 1) = 3;
+    W(1, 2) = 1;
+    Vector<3> b;
+    b[0] = 1;
+    b[1] = 2;
+    b[2] = 2;
+
+    Linear<2, 3> l;
+    l.W = W;
+    l.b = b;
+
+    Matrix<2, 2> src;
+    src(0, 0) = -1;
+    src(0, 1) = -2;
+    src(1, 0) = 0.5;
+    src(1, 1) = -0.5;
+    auto ret = l.forward(src);
+
+    Matrix<2, 3> ans = src.dot(W) + b;
+    return std::make_tuple(ret, ans);
+}
+
+constexpr void test_linear1()
+{
+    constexpr auto ret = test_linear1_detail();
+    static_assert(std::get<0>(ret) == std::get<1>(ret));
+}
+
+constexpr auto test_relu_detail()
+{
+    Matrix<2, 2> mat, mat2;
+    mat(0, 0) = -1;
+    mat(0, 1) = -2;
+    mat(1, 0) = 0.5;
+    mat(1, 1) = -0.5;
+    mat2(0, 0) = 0;
+    mat2(0, 1) = 0;
+    mat2(1, 0) = 0.5;
+    mat2(1, 1) = -0;
+
+    ReLU<2> relu;
+
+    return std::make_tuple(relu.forward(mat), mat2);
+}
+
+constexpr void test_relu()
+{
+    constexpr auto ret = test_relu_detail();
+    static_assert(std::abs(std::get<0>(ret)(0, 0) - std::get<1>(ret)(0, 0)) <
+                  1e-5);
+    static_assert(std::abs(std::get<0>(ret)(0, 1) - std::get<1>(ret)(0, 1)) <
+                  1e-5);
+    static_assert(std::abs(std::get<0>(ret)(1, 0) - std::get<1>(ret)(1, 0)) <
+                  1e-5);
+    static_assert(std::abs(std::get<0>(ret)(1, 1) - std::get<1>(ret)(1, 1)) <
+                  1e-5);
+}
+
+constexpr auto test_softmax_cross_entropy_detail()
+{
+    Matrix<2, 2> mat, mat2;
+    mat(0, 0) = -1;
+    mat(0, 1) = -2;
+    mat(1, 0) = 0.5;
+    mat(1, 1) = -0.5;
+    mat2(0, 0) = 0;
+    mat2(0, 1) = 1;
+    mat2(1, 0) = 1;
+    mat2(1, 1) = 0;
+
+    SoftmaxCrossEntropy<2> sce;
+    return std::make_tuple(sce.forward(mat, mat2), 0.8132616281509399);
+}
+
+constexpr void test_softmax_cross_entropy()
+{
+    constexpr auto ret = test_softmax_cross_entropy_detail();
+    static_assert(std::abs(std::get<0>(ret) - std::get<1>(ret)) < 1e-5);
 }
